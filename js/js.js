181b4810154e896aac4919be16f07d4064c10ef7
alexpodolin <alex.podolin@gmail.com>
2017-04-26 09:36:40 +0300

    js

--- js/js.js
+++ js/js.js
@@ -1,10 +1,24 @@
 'use strict';
 
+//Показать скрытое под-меню слева
+function showLeftSubmenu(thisel) {
+	
+	var subMenu = document.getElementsByClassName('main__menu_nested'),
+		i = 0;
+	for(i; i < subMenu.length; i++ )
+	{
+		subMenu[i].className = subMenu[i].className.replace('main__menu_nested_show', '').trim();
+	}
+
+// добавим в css class="main__menu_nested_show" тому элементу, куда кликаем
+thisel.parentNode.getElementsByClassName('main__menu_nested')[0].className += ' main__menu_nested_show';
+}
+
 //Появление меню при нажатии кнопки на разрешении < 480px
-/*function showMenuHeader() {
+function showMenuHeader() {
 	document.getElementsByClassName('header__menu')[0].style.display = "block";
 	document.getElementsByClassName('header__menu')[0].style.margin = "5px 0 0 0";
-}*/
+}
 
 // function showShip(); {
 // 	 document.getElementsByClassName('main__menu_ship_nav')[0].style.display = "block";
@@ -28,3 +42,5 @@
 
 // window.addEventListener('load', rmLinkAlignment, false);
 
+///////////////////////////////////////////////////////////////
+// use wordpress jquery
