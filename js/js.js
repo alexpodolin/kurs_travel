a87d14c3b77430f091eb76fd483b2908ef8b04f5
alexpodolin <alex.podolin@gmail.com>
2017-05-20 20:15:46 +0300

    qe

--- js/js.js
+++ js/js.js
@@ -1,8 +1,7 @@
 'use strict';
 
 //Показать скрытое под-меню слева
-function showLeftSubmenu(thisel) {
-	
+function showLeftSubmenu(thisel) {	
 	var subMenu = document.getElementsByClassName('main__menu_nested'),
 		i = 0;
 	for(i; i < subMenu.length; i++ )
@@ -14,33 +13,29 @@ function showLeftSubmenu(thisel) {
 thisel.parentNode.getElementsByClassName('main__menu_nested')[0].className += ' main__menu_nested_show';
 }
 
+
+// Меню для страницы теплоходов
+function showShipMenu(thisel) {
+	var subMenuShip = document.getElementsByClassName('main__menu_ship_nav'),
+		i = 0;
+	for(i; i < subMenuShip.length; i++ )
+	{
+		subMenuShip[i].className = subMenuShip[i].className.replace('main__menu_ship_nav_show', '').trim();
+	}
+
+// добавим в css class="main__menu_ship_nav_show" потомку элемента, куда кликаем
+thisel.parentNode.getElementsByClassName('main__menu_ship_nav')[0].className += ' .main__menu_ship_nav_show';
+}
+
 //Появление меню при нажатии кнопки на разрешении < 480px
 function showMenuHeader() {
 	document.getElementsByClassName('header__menu')[0].style.display = "block";
 	document.getElementsByClassName('header__menu')[0].style.margin = "5px 0 0 0";
 }
 
-// function showShip(); {
-// 	 document.getElementsByClassName('main__menu_ship_nav')[0].style.display = "block";
-// }
-//Уберем класс в меню для 
-//корректного отображения при разрешении <= 480px
-// function rmLinkAlignment() {
-// 	var scrWidth = screen.width;
-
-// 	if (scrWidth <= 480) {
-// 		var menuLinkEl = document.getElementsByClassName('main__menu_basic_link_alignment');
-
-// 		 for (var i = 0; i < menuLinkEl.length; i++) {
-// 			menuLinkEl[0].className = menuLinkEl[0].className.replace(' main__menu_basic_link_alignment', ' ');		 	
-// 		 }
-// 	}
-// }
-
-// rmLinkAlignment();
-
-
-// window.addEventListener('load', rmLinkAlignment, false);
-
 ///////////////////////////////////////////////////////////////
 // use wordpress jquery
+
+function addShipSubmenu(el) {
+	console.log(el);
+}
